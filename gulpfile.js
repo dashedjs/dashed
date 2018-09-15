f239814c57e884e78f394a3d443c2057f0b571f1
Fidèle PALOUKI <fidele.palouki@gmail.com>
2018-09-16 01:18:14 +0200

    add gulp tasks for builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const rollup = require('rollup');
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-const minify = require('gulp-babel-minify');
-const del = require('del');
+const rollup = require('rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const terser = require('rollup-plugin-terser');
+const replace = require('rollup-plugin-replace');
 
 const packages = [
   'button',
@@ -24,22 +24,77 @@ const packages = [
   'toggle',
   'utils'
 ];
-const paths = packages.map(package => ({
-  src: `packages/${package}/${package}.js`,
-  dest: `packages/${package}/${package}.min.js`
-}));
 
-gulp.task('build', build);
+gulp.task('buildSeries', buildSeries);
+gulp.task('buildParallel', buildParallel);
+gulp.task('buildBundle', buildBundle);
 
-function build() {
-  return gulp
-    .src(paths[0].src)
-    .pipe(
-      minify({
-        mangle: {
-          keepClassName: true
-        }
+function buildBundle() {
+  return rollup
+    .rollup({
+      input: 'packages/index.js',
+      plugins: [
+        resolve(),
+        terser.terser({
+          module: true,
+          mangle: {
+            module: true
+          }
+        })
+      ]
+    })
+    .then(rollupFileBuild =>
+      rollupFileBuild.write({
+        file: 'dist/index.min.js',
+        format: 'es'
       })
+    );
+}
+
+async function buildSeries() {
+  for (let package of packages) {
+    const rollupFileBuild = await rollup.rollup({
+      input: `packages/${package}/${package}.js`,
+      plugins: [
+        resolve(),
+        terser.terser({
+          module: true,
+          mangle: {
+            module: true
+          }
+        })
+      ]
+    });
+    console.log(`Building package: ${package} ...`);
+    rollupFileBuild.write({
+      file: `packages/${package}/dist/${package}.min.js`,
+      format: 'es'
+    });
+  }
+}
+
+async function buildParallel() {
+  await Promise.all(
+    packages.map(package =>
+      rollup
+        .rollup({
+          input: `packages/${package}/${package}.js`,
+          plugins: [
+            resolve(),
+            terser.terser({
+              module: true,
+              mangle: {
+                module: true
+              }
+            })
+          ]
+        })
+        .then(rollupFileBuild =>
+          rollupFileBuild.write({
+            file: `packages/${package}/dist/${package}.min.js`,
+            format: 'es'
+          })
+        )
     )
-    .pipe(gulp.dest(paths[0].dest));
+  );
 }
