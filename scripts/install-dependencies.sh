39bcf2d235f6609f20ad90df2068c07c9676bbd4
Fidèle PALOUKI <fidele.palouki@gmail.com>
2018-11-07 20:06:14 +0100

    add script for link dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,9 +2,34 @@
 
 set -e
 
-packagesFolders=(`ls ../packages`)
-for packageFolder in ${packagesFolders[@]}; do
-  echo ${packageFolder}
+sharedPackages=`ls packages`
+for sharedPackage in ${sharedPackages[@]}; do
+  if [[ ${sharedPackage} == utils || ${sharedPackage} == styles || ${sharedPackage} == icons ]]; then
+    cd packages/${sharedPackage}
+    npm link
+    cd ../..
+  fi
 done
 
-echo 'hello world'
+packagesFolders=`ls packages`
+echo ${packageFolders[@]}
+for packageFolder in ${packagesFolders[@]}; do
+  if [[ ${packageFolder} == utils || ${packageFolder} == styles || ${packageFolder} == icons || ${packageFolder} == index.js ]]; then
+    echo "skipping ${packageFolder}"
+  elif [[ ${packageFolder} == icon ]]; then
+    cd packages/${packageFolder}
+    npm link @dashedjs/dashed-icons
+    npm link
+    cd ../../
+  elif [[ ${packageFolder} == header ]]; then
+    cd packages/${packageFolder}
+    npm link @dashedjs/dashed-icon @dashedjs/dashed-icons @dashedjs/dashed-utils @dashedjs/dashed-styles
+    npm link
+    cd ../../
+  else
+    cd packages/${packageFolder}
+    npm link @dashedjs/dashed-utils @dashedjs/dashed-styles
+    npm link
+    cd ../../
+  fi 
+done
